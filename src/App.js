e4e1f475ef2236039d243f350316b835bd436085
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-06-25 17:16:37 +0100

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 
+import Dictionary from "./Dictionary";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">Coded by Gemma Allwright</footer>
+      </div>
     </div>
   );
 }
