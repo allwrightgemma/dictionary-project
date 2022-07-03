7a5ab9d4d66321ed30f67b7e6f61a2c1d8997285
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-07-03 13:33:14 +0100

    Added photos api and displayed photos

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Dictionary</h1>
-        </header>
+        <header className="App-header"></header>
         <main>
           <Dictionary defaultKeyword="dictionary" />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    let pexelsApiKey =
+      "563492ad6f917000010000019ebd88495fa44ba3a465f12e7ef20915";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -43,9 +54,10 @@ export default function Dictionary(props) {
               placeholder="Search for a word"
             />
           </form>
-          <div className="hint">suggested words: evolve, happy, twinge</div>
+          <div className="hint">suggested words: sunset, happy, cheese</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
