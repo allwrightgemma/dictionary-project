f3250db078dcb705eb856af44ce95c5b8057d505
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2023-01-27 16:13:10 +0000

    Adjusted styling and logo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import "./App.css";
-import logo from "./logo.png";
+import './App.css';
 
-import Dictionary from "./Dictionary";
+import Dictionary from './Dictionary';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <img src={logo} alt="Owl with book logo" className="logo img-fluid" />
-        </header>
-        <main>
-          <Dictionary defaultKeyword="dictionary" />
-        </main>
+	return (
+		<div className='App'>
+			<div className='container'>
+				<header className='App-header'>
+					<i class='fa-solid fa-book-open logo'></i>{' '}
+					{/* <img src={logo} alt='Owl with book logo' className='logo img-fluid' /> */}
+				</header>
+				<main>
+					<Dictionary defaultKeyword='dictionary' />
+				</main>
 
-        <footer className="App-footer">
-          <a href="https://github.com/allwrightgemma/dictionary-project">
-            Coded by Gemma Allwright
-          </a>
-        </footer>
-      </div>
-    </div>
-  );
+				<footer className='App-footer'>
+					<a href='https://github.com/allwrightgemma/dictionary-project'>
+						Coded by Gemma Allwright
+					</a>
+				</footer>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,67 +1,67 @@
-import React, { useState } from "react";
-import "./Dictionary.css";
-import axios from "axios";
-import Results from "./Results";
-import Photos from "./Photos";
+import React, { useState } from 'react';
+import './Dictionary.css';
+import axios from 'axios';
+import Results from './Results';
+import Photos from './Photos';
 
 export default function Dictionary(props) {
-  const [keyword, setKeyword] = useState(props.defaultKeyword);
-  const [results, setResults] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-  const [photos, setPhotos] = useState(null);
+	const [keyword, setKeyword] = useState(props.defaultKeyword);
+	const [results, setResults] = useState(null);
+	const [loaded, setLoaded] = useState(false);
+	const [photos, setPhotos] = useState(null);
 
-  function handleDictionaryResponse(response) {
-    setResults(response.data[0]);
-  }
+	function handleDictionaryResponse(response) {
+		setResults(response.data[0]);
+	}
 
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
-  }
+	function handlePexelsResponse(response) {
+		setPhotos(response.data.photos);
+	}
 
-  function search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleDictionaryResponse);
-    let pexelsApiKey =
-      "563492ad6f917000010000019ebd88495fa44ba3a465f12e7ef20915";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
-  }
+	function search() {
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleDictionaryResponse);
+		let pexelsApiKey =
+			'563492ad6f917000010000019ebd88495fa44ba3a465f12e7ef20915';
+		let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+		let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+		axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+	}
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
+	}
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
+	function handleKeywordChange(event) {
+		setKeyword(event.target.value);
+	}
 
-  function load() {
-    setLoaded(true);
-    search();
-  }
+	function load() {
+		setLoaded(true);
+		search();
+	}
 
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <section>
-          <h1> What word do you want to look up? </h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              onChange={handleKeywordChange}
-              placeholder="Search for a word"
-            />
-          </form>
-          <div className="hint">suggested words: sunset, happy, cheese</div>
-        </section>
-        <Results results={results} />
-        <Photos photos={photos} />
-      </div>
-    );
-  } else {
-    load();
-    return "loading";
-  }
+	if (loaded) {
+		return (
+			<div className='Dictionary'>
+				<section>
+					<h1> What word do you want to look up? </h1>
+					<form onSubmit={handleSubmit}>
+						<input
+							type='search'
+							onChange={handleKeywordChange}
+							placeholder='Search for a word'
+						/>
+					</form>
+					<div className='hint'>Suggested words: sunset, happy, cheese</div>
+				</section>
+				<Results results={results} />
+				<Photos photos={photos} />
+			</div>
+		);
+	} else {
+		load();
+		return 'loading';
+	}
 }
